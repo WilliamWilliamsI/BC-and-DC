d2ff3b6a2207f9529d3d90cdc400a687724d68f8
KarryRen <KarryRenKai@outlook.com>
2025-03-08 12:29:03 +0800

    add: add h1 project

---
+++ Homeworks/Homework_1/H1_Proejct/README.md
@@ -0,0 +1,3 @@
+# H1 Project
+
+对 Homework 1 的工程性文件做说明
--- Homeworks/Homework_1/H1_Proejct/src/Main.java
+++ Homeworks/Homework_1/H1_Proejct/src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * @Time : 2025/3/8 12:27
+ * @Author : Karry Ren
+ * @Comment:
+ **/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
