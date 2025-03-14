b04f966dbe8017ffadd8fe5e69158d3b15bafc2a
KarryRen <KarryRenKai@outlook.com>
2025-03-14 17:02:01 +0800

    init: init the P2

---
+++ Homeworks/Homework_2/H2_Project/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Time : 2025/3/14 14:05
+ * @Author : Karry Ren
+ * @Comment:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
