5cb2815d7db9802603fdbe84e950665d8ebd8906
KarryRen <KarryRenKai@outlook.com>
2025-04-24 22:46:57 +0800

    add: add sing-in up (#6)

--- Project/Code/paobuliao-front-end/src/main.js
+++ Project/Code/paobuliao-front-end/src/main.js
@@ -1,4 +1,22 @@
+// @Author : Kai Ren
+// @Time   : 2025.03.21
+// @Comment: The entrance of the whole application.
+
+// basic APP import
 import {createApp} from 'vue'
 import App from './App.vue'
+// element plus import
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/dist/locale/zh-cn'
+// append css
+import './assets/main.scss'
 
-createApp(App).mount('#app')
+// Step 1. Creat the APP
+const app = createApp(App)
+
+// Step 2. Use Element Plus to set APP
+app.use(ElementPlus, {locale})
+
+// Step 3. Run the APP
+app.mount('#app')
